51045e575a2cf43171b50864562eab6018dd5965
nitzmahone <mdavis@ansible.com>
2016-09-21 10:10:12 -0700

    updates

---
+++ library/win_exchange_componentstate.ps1
@@ -0,0 +1,33 @@
+#!powershell
+
+# POWERSHELL_COMMON
+# WANTS_JSON
+
+Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+
+$parsed_args = Parse-Args $args
+
+$result = @{changed=$false}
+
+$component_name = Get-AnsibleParam $parsed_args "component_name" -default "ServerWideOffline"
+$state = Get-AnsibleParam $parsed_args "state" -failifempty $result -validateset "active","draining"
+
+$cu = "http://$($env:COMPUTERNAME).$($env:USERDNSDOMAIN)/Powershell"
+
+Try {
+  $gcs_result = Invoke-Command -ConnectionUri $cu -Authentication Kerberos -ConfigurationName Microsoft.Exchange -ScriptBlock { Get-ServerComponentState -Identity $args[0] } -Args $env:COMPUTERNAME,$component_name
+
+  $current_state = $gcs_result.State
+  
+  If($current_state -ne $state) {
+    $scs_result = Invoke-Command -ConnectionUri $cu -Authentication Kerberos -ConfigurationName Microsoft.Exchange -ScriptBlock { Set-ServerComponentState -Identity $args[0] -Component $args[1] -State $args[2] -RequesterMaintenance } -Args $env:COMPUTERNAME,$component_name,$state
+    $result.changed = $true
+  }
+}
+Catch {
+  $result = @{failed=$true; msg="Exchange Management unavailable: $_"}
+}
+
+ConvertTo-Json $result -Depth 99
+
